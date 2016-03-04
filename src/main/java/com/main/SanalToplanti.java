ebfe970d0aae194be6abe62e436dda3bb2143bf1
Burak Demirci <Burak Demirci>
2016-03-04 16:39:47 +0200

    Proje, standart java projesinden maven projesine dönüştürüldü.

---
+++ src/main/java/com/main/SanalToplanti.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.main;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Burak
+ */
+public class SanalToplanti {
+    public static void main(String[] args){
+    JFrame pencere = new JFrame();
+    pencere.setSize(100, 100);
+    pencere.setVisible(true);
+    }
+}
