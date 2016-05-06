e02398ca8488e2390daa4656cfd98624c96d263f
Burak Demirci <Burak Demirci>
2016-05-06 18:43:42 +0300

    yeni proje webrtc

---
+++ YazilimMuhendisligiOdev/YazilimMuhendisligiOdev/Models/Oda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YazilimMuhendisligiOdev.Models
+{
+    public class Oda
+    {
+        public int OdaID { get; set; }
+        public string Odaİsmi { get; set; }
+        ICollection<Kullanici> Kullanicilar { get; set; }
+    }
+}
