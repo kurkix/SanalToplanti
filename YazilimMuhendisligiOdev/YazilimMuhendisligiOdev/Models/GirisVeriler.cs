bba76f87cc4f1df3680133d41c7af38d7e96845f
Burak Demirci <Burak Demirci>
2016-05-07 00:00:03 +0300

    uygulama kısmı teması yapılıyor

---
+++ YazilimMuhendisligiOdev/YazilimMuhendisligiOdev/Models/GirisVeriler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YazilimMuhendisligiOdev.Models
+{
+    public class GirisVeriler
+    {
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+    }
+}
